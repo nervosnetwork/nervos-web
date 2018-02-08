b800a73adae65a29f6d04ae8842664d197ae100a
Keith <keithwhisper@gmail.com>
2018-02-08 15:58:34 +0800

    update: sticky footer to bottom

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,10 +1,3 @@
 import * as React from 'react'
-
-// export class HighEffeciency extends React.Component {
-//   render() {
-//     return <div>highEfficient</div>
-//   }
-// }
-
 export const HighEffeciency = () => <div>HighEffeciency</div>
 export const remainding = false
