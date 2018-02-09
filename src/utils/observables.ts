50f7f5e7938cfebcfeed83fdba413564472f3fb8
Keith <keithwhisper@gmail.com>
2018-02-09 18:17:50 +0800

    update: use @reactivex/rxjs instead of rxjs@alpha

--- src/utils/observables.ts
+++ src/utils/observables.ts
@@ -1,9 +1,6 @@
 import { Observable } from 'rxjs'
 import throttleTime from 'rxjs/add/operator/throttleTime'
 
-// const { fromEvent } = require('rxjs/create')
-// const { throttleTime } = require('rxjs/operators')
-
 export const scrollObservable = Observable.fromEvent(
   document,
   'scroll',
