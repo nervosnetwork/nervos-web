aafcbace68268edea9cc6f20ed5d9852a7ac9d1e
Terry Tai <poshboytl@gmail.com>
2018-03-14 22:41:27 +0800

    Remove the old desc.

--- src/config/locale/en.ts
+++ src/config/locale/en.ts
@@ -9,7 +9,7 @@ export default {
   slogan: {
     word: 'The Common Knowledge Base for The 7.6 Billion People.',
     desc:
-      'NERVOS facilitates human collaboration on the global scale, and serves as the protocol of the new digital commonwealth. It employs an on-chain protocol governance model so that the network can evolve to maximize the welfare of all participants.',
+      '???....',
   },
   features: {
     title: 'Features',
