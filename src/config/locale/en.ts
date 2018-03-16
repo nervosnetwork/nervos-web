081845a961fd032da0a96fc3aea3edc753701066
Kevin Wang <knwang@gmail.com>
2018-03-16 14:54:55 -0400

    Typo

--- src/config/locale/en.ts
+++ src/config/locale/en.ts
@@ -11,7 +11,7 @@ export default {
     word: 'The Trustware Platform for the 7.6 Billion People',
     desc: [
       `Nervos is a suite of scalable and interoperable protocols and an open blockchain "Trustware" platform for decentralized applications. Trustware is a new decentralized computing paradigm that goes beyond distributed ledgers and smart contracts, to bring general expression of trust to meet the demand of today's real world applications. `,
-      `In contrast to smart contract platforms, Nervos uses the "Common Knowledge Base", a simpler state machine with uniform state transation semantics for superior scalability, interoperability and better aligned incentives for all participants of the network.`,
+      `In contrast to smart contract platforms, Nervos uses the "Common Knowledge Base", a simpler state machine with uniform state transition semantics for superior scalability, interoperability and better aligned incentives for all participants of the network.`,
     ],
   },
   features: {
