5a1141c4e05b41d68e4901bd0ecbadd7f9dd452a
Yao <GyonGyon@users.noreply.github.com>
2018-04-26 18:17:01 +0800

    update no log in production

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,8 @@
-const log = console.log.bind(console, '>>>')
-
+const log = (...arg) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('>>>', ...arg)
+  }
+}
 // const pathFile = (name, suffix, dirPath) => `${dirPath + name}.${suffix}`
 
 // const pathCss = (name, suffix, dirPath = '../../styles/') =>
