4b22e750ee6763888986b84ba92776fb0f04497b
Yao <GyonGyon@users.noreply.github.com>
2018-07-17 14:15:19 +0800

    update: add investor

---
+++ Investors_files/investors.js
@@ -0,0 +1,79 @@
+var __investors = function () {
+  var log = console.log.bind('console')
+
+  var newElement = function (tag) {
+    return document.createElement(tag)
+  }
+
+  var newFragment = function () {
+    return document.createDocumentFragment()
+  }
+
+  var bind = function (element, eventName, callback) {
+    element.addEventListener(eventName, callback)
+  }
+
+  var unbind = function (element, eventName, callback) {
+    element.removeEventListener(eventName, callback)
+  }
+
+  var bindFadeinup = function (element) {
+    var e = element
+    var max = window.innerHeight
+    var min = 0
+    var cls = e.className
+    e.className = 'fadeinupBefore animation ' + cls
+    var func = function () {
+      var position = e.getBoundingClientRect()
+      var top = position.top
+      var bottom = position.bottom
+      if (top < max && bottom > min) {
+        e.className = 'fadeinup animation ' + cls
+        unbind(window, 'scroll', func)
+      }
+    }
+    bind(window, 'scroll', func)
+  }
+
+  var newInvestor = function (params) {
+    var link = newElement('a')
+    link.href = params.href
+    link.className = 'investor'
+
+    var img = newElement('img')
+    img.src = params.src
+    img.className = 'investorImg'
+
+    link.appendChild(img)
+    bindFadeinup(link)
+    return link
+  }
+
+  var insertInvestors = function () {
+    var frag = newFragment()
+    var imgs = g_ImgListInvestors
+    var size = imgs.length
+    for (var i = 0; i < size; i++) {
+      var info = imgs[i]
+      var e = newInvestor(info)
+      frag.appendChild(e)
+    }
+    var investors = document.getElementById('id-investors-imgs')
+    investors.appendChild(frag)
+  }
+
+  var afterDomloaded = function () {
+    insertInvestors()
+    g_ImgListInvestors = undefined
+    unbind(window, 'DOMContentLoaded', afterDomloaded)
+  }
+
+  var main = function () {
+    bind(window, 'DOMContentLoaded', afterDomloaded)
+  }
+
+  main()
+}
+
+__investors()
+__investors = undefined
---
+++ Investors_files/investors_imgs.js
@@ -0,0 +1,108 @@
+var newImgsrc = function (name) {
+  var path = './Investors_files/imgs/'
+  var src = path + name + '.png'
+  return src
+}
+
+var g_ImgListInvestors = [
+  {
+    href: '',
+    src: newImgsrc('alternity'),
+  },
+  {
+    href: '',
+    src: newImgsrc('blockchain'),
+  },
+  {
+    href: '',
+    src: newImgsrc('breyer'),
+  },
+  // {
+  //   href: '',
+  //   src: newImgsrc('bihu'),
+  // },
+  {
+    href: '',
+    src: newImgsrc('bixin'),
+  },
+  {
+    href: '',
+    src: newImgsrc('ceyuan'),
+  },
+  {
+    href: '',
+    src: newImgsrc('dekrypt'),
+  },
+  {
+    href: '',
+    src: newImgsrc('distributed'),
+  },
+  {
+    href: '',
+    src: newImgsrc('ddc'),
+  },
+  {
+    href: '',
+    src: newImgsrc('fbg'),
+  },
+  {
+    href: '',
+    src: newImgsrc('ff'),
+  },
+  {
+    href: '',
+    src: newImgsrc('huobi'),
+  },
+  {
+    href: '',
+    src: newImgsrc('sequoia'),
+  },
+  {
+    href: '',
+    src: newImgsrc('unity'),
+  },
+  {
+    href: '',
+    src: newImgsrc('martix'),
+  },
+  {
+    href: '',
+    src: newImgsrc('multi'),
+  },
+  {
+    href: '',
+    src: newImgsrc('1confirmation'),
+  },
+  {
+    href: '',
+    src: newImgsrc('1kx'),
+  },
+  {
+    href: '',
+    src: newImgsrc('passport'),
+  },
+  {
+    href: '',
+    src: newImgsrc('polychain'),
+  },
+  {
+    href: '',
+    src: newImgsrc('vy'),
+  },
+  {
+    href: '',
+    src: newImgsrc('wanxiang'),
+  },
+  {
+    href: '',
+    src: newImgsrc('spark'),
+  },
+  {
+    href: '',
+    src: newImgsrc('xianzai'),
+  },
+  {
+    href: '',
+    src: newImgsrc('zk'),
+  },
+]
